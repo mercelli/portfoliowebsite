97df919713c6922dae9391e04d802e34931871c3
mercelli <ermercelli@gmail.com>
2025-11-19 20:02:41 -0600

    Changed cmponent capital

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,5 @@
-import Section from "../Components/ui/Section.jsx";
-import Container from "../Components/ui/Container.jsx";
+import Section from "../components/ui/Section.jsx";
+import Container from "../components/ui/Container.jsx";
 import SEO from "../components/ui/SEO.jsx";
 
 export default function Resume() {
